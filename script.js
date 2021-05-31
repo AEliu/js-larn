ea946c5573c83a443a693567be646cae3ee85690
AEliu <95649311@qq.com>
2021-05-31 11:18:14 +0800

    update

--- script.js
+++ script.js
@@ -34,3 +34,43 @@ window.document.writeln(result);
 // Uncaught ReferenceError: sum is not defined
 // console.log(sum);
 
+//  原始值包装类型
+// ECMAScript提供了3种特殊的引用类型：Boolean、Number和String
+
+let s1 = "some text";
+let s2 = s1.substring(2);
+
+console.log(s2);
+// me text
+
+// 引用类型与原始值包装类型的生命周期
+
+s1.color = "red";
+console.log(s1.color); // undefined
+
+let s3 = new String("other text");
+s3.color = "green";
+console.log(s3.color); // green
+console.log(typeof s3); // object
+
+let num = 10.24;
+console.log(num.toFixed(1)); // 10.2, 四舍五入
+console.log(num.toFixed(3)); // 10.240
+console.log(num.toExponential(1)); // 1.0e+1
+console.log(num.toExponential(3)); // 1.024e+1
+
+let stringObject=new String("hello world");
+console.log(stringObject.length); // 11
+console.log(stringObject.charAt(3)); // l
+console.log(stringObject.charCodeAt(3)); // 108
+console.log(stringObject.startsWith('h')); // true
+console.log(stringObject.repeat(3)); // hello worldhello worldhello world
+
+let wikiurl = 'https://zh.wikipedia.org/wiki/字符编码#现代编码模型';
+console.log(encodeURI(wikiurl));
+console.log(encodeURIComponent(wikiurl)); // 将所有非字母字符都替换成了相应的编码形式
+
+            
+eval("console.log('hi')");       
+
+console.log(typeof window);
